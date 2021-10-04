84b0ce2fc28577fc2f75bf2194454ac4d32d9f84
HojeongPark <parkhj062@gmail.com>
2021-10-04 22:44:28 +0900

    # 회원가입, 로그인 구현

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,10 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { AuthenticationError, ForbiddenError } = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../util/gravatar');
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -25,5 +32,43 @@ module.exports = {
         new: true,
       },
     );
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+    email = email.trim().toLowerCase();
+    const hashed = await bcrypt.hash(password, 10);
+    const avatar = gravatar(email);
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed,
+      });
+
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+      throw new Error('Error creating account');
+    }
+  },
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (!user) {
+      throw new AuthenticationError('Error signing in');
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError('Error signing in');
+    }
+
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   },
 };
